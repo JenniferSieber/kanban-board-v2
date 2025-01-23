3a7f3d1f52ce056e3b03d0012ba744e187db2f66
JenniferSieber <sieberjen6@gmail.com>
2025-01-23 16:18:19 -0700

    add logic for onDragEnd swapped columns

--- src/components/KanbanBoard.tsx
+++ src/components/KanbanBoard.tsx
@@ -2,8 +2,8 @@ import { Column, Id } from "../types";
 import { useState, useMemo } from "react";
 import PlusIcon from "../icons/PlusIcon";
 import ColumnContainer from "./ColumnContainer";
-import { DndContext, DragOverlay, DragStartEvent } from "@dnd-kit/core";
-import { SortableContext } from "@dnd-kit/sortable";
+import { DndContext, DragOverlay, DragStartEvent, DragEndEvent } from "@dnd-kit/core";
+import { SortableContext, arrayMove } from "@dnd-kit/sortable";
 import { createPortal } from "react-dom";
 
 function KanbanBoard() {
@@ -36,9 +36,30 @@ function KanbanBoard() {
     }
   };
 
+  const onDragEnd = (event: DragEndEvent) => {
+    console.log(event)
+    const {active, over} = event;
+
+    if (!over) return; // not dragging over a valid element
+
+    const activeColumnId = active.id;
+    const overColumnId = over.id;
+    if (activeColumnId === overColumnId) return; // same column
+  
+    const swappedColumns = (columns: Column[]) => {
+      const activeColumnIndex = columns.findIndex(col => col.id === activeColumnId)
+      const overColumnIndex = columns.findIndex(col => col.id === overColumnId)
+      return arrayMove(columns, activeColumnIndex, overColumnIndex)
+    }
+    setColumns(swappedColumns)
+  }
+
   return (
     <div className="m-auto flex min-h-screen w-full items-center overflow-x-auto overflow-y-hidden px-[40px]">
-      <DndContext onDragStart={onDragStart}>
+      <DndContext 
+        onDragStart={onDragStart}
+        onDragEnd={onDragEnd}
+      >
         <div className="m-auto flex flex-col gap-4">
           <div className="flex gap-4">
             <SortableContext items={columnsId}>
